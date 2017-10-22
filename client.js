55d60d202e8b922c055979c779dd321e191fe942
ElenaAtluri <31358148+ElenaAtluri@users.noreply.github.com>
2017-10-21 19:15:59 -0700

    Add files via upload
    Cubes galore :)

--- client.js
+++ client.js
@@ -1,61 +1,11 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-var geometry = new THREE.PlaneGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial( { color: 0xc6dcff} );
-var plane = new THREE.Mesh( geometry, material );
-scene.add( plane );
-
-camera.position.z = 5
-
-var animate = function () {
-	requestAnimationFrame( animate );
-		 plane.position.x += 0.12;
-		 plane.position.z -= 0.08;
-		 plane.rotation.x += 0.005;
-		 plane.rotation.y += 0.005;
-		
-
-	renderer.render( scene, camera );
-};
-
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-
-function onMouseMove( event ) {
-
-	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
-}
-
-function render() {
-
-	// update the picking ray with the camera and mouse position
-	raycaster.setFromCamera( mouse, camera );
-
-	// calculate objects intersecting the picking ray
-	var intersects = raycaster.intersectObject( plane );
-
-	if ( intersects == true ) {
-
-		intersects[ i ].object.material.color.set( 0xff0000 );
-
-	}
-
-	renderer.render( scene, camera );
-
-}
-
-window.addEventListener( 'mousemove', onMouseMove, false );
-
-window.requestAnimationFrame(render);
-
-animate();
+$(function () {
+  var socket = io();
+  $('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+  });
+  socket.on('chat message', function(msg){
+    $('#messages').append($('<li>').text(msg));
+  });
+});
--- index.js
+++ index.js
@@ -1,36 +1,50 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var THREE = require("./src/three.js");
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var userNum = 0;
+var cubes = [];
 
-io.on('connection', function(socket){
-  var addedUser = false;
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+io.on('connection', function(socket) {
+  socket.on('add object', function(obj){
+    console.log("creating object at " + obj.x + ", " + obj.y + ", " + obj.z + ", with color " + obj.color);
+    io.sockets.emit('add object', {x:obj.x, y:obj.y, z:obj.z, color:obj.color});
   });
-  socket.on('get user', function(username){
-    if (addedUser) {
-      io.emit('added user', username);
-      return;
-    }
-    if (username.trim() === "") username="Anonymous";
-    socket.username = username;
-    ++userNum;
-    addedUser = true;
-    io.emit('get user', username);
-    io.emit('added user', username);
-  });
-  socket.on('chat message', function(msg){
-    console.log(socket.username + ': ' + msg);
-    io.emit('chat message', msg);
+  socket.on('cubes', function(obj){
+    cubes.push(obj);
+    io.sockets.emit('cubes', cubes);
   });
 });
+
+// var userNum = 0;
+
+// io.on('connection', function(socket){
+//   var addedUser = false;
+//   console.log('a user connected');
+//   socket.on('disconnect', function(){
+//     console.log('user disconnected');
+//   });
+//   socket.on('get user', function(username){
+//     if (addedUser) {
+//       io.emit('added user', username);
+//       return;
+//     }
+//     if (username.trim() === "") username="Anonymous";
+//     socket.username = username;
+//     ++userNum;
+//     addedUser = true;
+//     io.emit('get user', username);
+//     io.emit('added user', username);
+//   });
+//   socket.on('chat message', function(msg){
+//     console.log(socket.username + ': ' + msg);
+//     io.emit('chat message', msg);
+//   });
+// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
